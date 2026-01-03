4b9e26e0571928c74303b4bfd655d1aed7287abe
abdul <59056864+asloma1984@users.noreply.github.com>
2026-01-03 16:30:10 +0000

    Add files via upload

---
+++ ssh/cf.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# System Requirements : Debian 9–13 / Ubuntu 18–25
+# Developer » Abdul (NorthAfrica Script)
+# Channel   » https://t.me/northafrica9
+# Group     » https://t.me/groupnorthafrica
+#
+# Stable Edition - Recode 2025
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+clear
+read -rp "Input Your Domain For This Server :" -e SUB_DOMAIN
+echo "Host : $SUB_DOMAIN"
+echo $SUB_DOMAIN > /root/domain
+# / / Make Main Directory
+mkdir -p /usr/bin/xray
+mkdir -p /etc/xray
+cp /root/domain /etc/xray
+rm -f /root/cf.sh
